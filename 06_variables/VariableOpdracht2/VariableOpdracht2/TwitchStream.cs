3cf8ec3a950230dc9b5ce9c744c26cda5276a830
Reko <36913@ma-web.nl>
2024-03-04 15:11:39 +0100

    opdr 6

--- 06_variables/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
+++ 06_variables/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -4,12 +4,21 @@ namespace VariableOpdracht2
 {
     internal class TwitchStream
     {
+        
+        
+          internal  string naam = "Warn";
+          internal string lastStreamName = "#1 Ashe";
+          internal Game streamGame = new Game("Overwatch");
+          internal int followers = 139900;
+          internal bool automod = false;
+        
+
         //maak hier class variables aan:
         // type=string, naam: name,
         // type=string, naam: lastStreamName
         // type=Game, naam: streamGame
         // type=int, naam: followers
         // type=bool, naam: automod
-        
+
     }
 }
