4ced7bcb61f618cdcb9f4225bac6d3280249e468
Reko <36913@ma-web.nl>
2024-03-07 12:18:21 +0100

    opdr 7

--- 07_rekenen/CalculateOpdracht2/CalculateOpdracht2/Program.cs
+++ 07_rekenen/CalculateOpdracht2/CalculateOpdracht2/Program.cs
@@ -11,6 +11,7 @@ namespace CalculateOpdracht2
             //we gaan nu dingen kopen
 
             int controllersInMandje = 0;
+            controllersInMandje = controllersInMandje + 2;
 
 
             //koop 2 controllers
@@ -18,6 +19,7 @@ namespace CalculateOpdracht2
 
             //hij is in voorraad snel kopen!
             int ps5 = 0;
+            ps5 = ps5 + 1;
 
             //koop 1 ps5
 
@@ -25,15 +27,18 @@ namespace CalculateOpdracht2
             //koop de games:
 
             int eldenRing = 0;
+            eldenRing = eldenRing + 1;
 
             int shantae = 0;
+            shantae = shantae + 1;
 
             int spongebob = 0;
+            spongebob = spongebob + 1;
 
 
             //hmmm weten we het zeker?
             //haal 1 game weg met -- of -= 1
-
+            shantae -= 1;
             //nu nog de prijs
 
 
@@ -42,12 +47,13 @@ namespace CalculateOpdracht2
             double gamePrijs = 59.99;
 
 
-            double totaalControllerPrijs = 0;// controllersInMandje keer controllerPrijs
-            double totaalGamePrijs = 0;// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
+            double totaalControllerPrijs = controllerPrijs * controllersInMandje;// controllersInMandje keer controllerPrijs
+            double totaalGamePrijs = gamePrijs * (eldenRing + shantae + spongebob);// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
 
 
 
-            double totaalPrijs = 0;// tel hier alles bij elkaar op
+            double totaalPrijs = totaalControllerPrijs + totaalGamePrijs + ps5Prijs;// tel hier alles bij elkaar op
+            Console.WriteLine(totaalPrijs);
 
             //er moet nu 759.95 staan
         }
