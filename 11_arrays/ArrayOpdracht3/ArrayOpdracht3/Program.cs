d1a48235e4833288f7fa3a6cb7a4fa11059cb73c
Reko <36913@ma-web.nl>
2024-03-28 12:17:01 +0100

    opdr11

--- 11_arrays/ArrayOpdracht3/ArrayOpdracht3/Program.cs
+++ 11_arrays/ArrayOpdracht3/ArrayOpdracht3/Program.cs
@@ -13,6 +13,13 @@ namespace ArrayOpdracht3
 
             Console.WriteLine(nameparts[0] + nameparts[2]);
 
+            Console.WriteLine(nameparts[4] + nameparts[1]);
+
+            Console.WriteLine(nameparts[3] + nameparts[5]);
+
+            Console.WriteLine(nameparts[1] + nameparts[0]);
+
+            Console.WriteLine(nameparts[3] + nameparts[5] + nameparts[2]);
             //maak nu de volgende namen en schrijf het naar de console:
 
             //link
